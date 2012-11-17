8582bc5250e6bd83a43615218e7a21f36177501e
Tamas Szekeres <szekerest@gmail.com>
2012-11-17 19:09:41 +0100

    Update stable branches for gdal 1.9.2 (#3) and mapserver 6.2.0 (#4)

--- setversions.bat
+++ setversions.bat
@@ -1,12 +1,12 @@
-set ms_version=6-0
-set ms_rel=3
-set ms_rev=2e17a60cd3c9e615a7f2f9104bdc60bba8198b89
+set ms_version=6-2
+set ms_rel=0
+set ms_rev=6c9d17a2ca188e552703d472de2b57f2ea7a98d4
 
 set gdal_major=1
 set gdal_minor=9
 set gdal_stable_tag=19
 set gdal_dev_tag=19
-set gdal_rel=1
-set gdal_rev=24429
+set gdal_rel=2
+set gdal_rev=25088
 
 set gdal_version=%gdal_major%.%gdal_minor%
