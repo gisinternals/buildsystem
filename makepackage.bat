c729a8f650fdcf7ef23b8e13d5b0d57b7ff93d73
Tamas Szekeres <szekerest@gmail.com>
2013-03-21 22:49:11 +0100

    Switch on msautotest support

--- makepackage.bat
+++ makepackage.bat
@@ -278,7 +278,7 @@ echo ^<span style="background-color:Lime;color:Black;font-weight:bold"^>%cmdname
 echo ^<span style="background-color:Lime;color:Black;font-weight:bold"^>Success^</span^>^<br/^> >>C:\Inetpub\wwwroot\sdk\build-output\%logid%.html
 )
 
-set nmakecmd=op-disable MS_DIR=..\sdk\%compiler%\%ms-dir% MSAUTOTEST_DIR=..\sdk\%compiler%\%msautotest-dir%
+set nmakecmd=ms-autotest MS_DIR=..\sdk\%compiler%\%ms-dir% MSAUTOTEST_DIR=..\sdk\%compiler%\%msautotest-dir%
 set cmdname=MapServer autotest
 for /f "tokens=1-7 delims=:.,- " %%a in ("%date% %time%") do set id=%%a%%b%%c-%%d-%%e-%%f-%%g-%logid%
 echo %date% %time%: %cmdname% started ^&nbsp^<a href="http://www.gisinternals.com/sdk/build-output/%compiler%-%id%.txt"^>stdout^</a^>^&nbsp^<a href="http://www.gisinternals.com/sdk/build-output/%compiler%-%id%-err.txt"^>stderr^</a^>^&nbsp >>C:\Inetpub\wwwroot\sdk\build-output\%logid%.html
