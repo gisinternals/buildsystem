e2ebdde2f8f2d0c336e4d8882599ac6b08764acc
Tamas Szekeres <szekerest@gmail.com>
2013-05-02 08:27:48 +0200

    Prepare buildsystem to use GDAL 1.10 stable branch (#24)

--- setversions.bat
+++ setversions.bat
@@ -1,12 +1,12 @@
 set ms_version=6-2
-set ms_rel=0
-set ms_rev=6c9d17a2ca188e552703d472de2b57f2ea7a98d4
+set ms_rel=1
+set ms_rev=3b531be271a1a01701e92eb7029d1914df466872
 
 set gdal_major=1
-set gdal_minor=9
-set gdal_stable_tag=19
-set gdal_dev_tag=19
-set gdal_rel=2
-set gdal_rev=25088
+set gdal_minor=10
+set gdal_stable_tag=110
+set gdal_dev_tag=110
+set gdal_rel=0
+set gdal_rev=25947
 
 set gdal_version=%gdal_major%.%gdal_minor%
