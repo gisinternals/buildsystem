e7a9258ba5151dd5964b98d73bbf60512f63093f
Tamas Szekeres <szekerest@gmail.com>
2013-06-02 11:41:00 +0200

    Can't launch SdkShell.bat (#27)

--- GDALShell.bat
+++ GDALShell.bat
@@ -2,8 +2,8 @@
 @echo Setting environment for using the GDAL Utilities.
 set SDK_ROOT=%~dp0
 set SDK_ROOT=%SDK_ROOT:\\=\%
-SET PATH=%SDK_ROOT%;%PATH%
-SET GDAL_DATA=%SDK_ROOT%gdal-data
-SET GDAL_DRIVER_PATH=%SDK_ROOT%gdalplugins
-SET PROJ_LIB=%SDK_ROOT%projlib
-SET PYTHONPATH=%SDK_ROOT%python
+SET "PATH=%SDK_ROOT%;%PATH%"
+SET "GDAL_DATA=%SDK_ROOT%gdal-data"
+SET "GDAL_DRIVER_PATH=%SDK_ROOT%gdalplugins"
+SET "PROJ_LIB=%SDK_ROOT%projlib"
+SET "PYTHONPATH=%SDK_ROOT%python"
--- SDKShell.bat
+++ SDKShell.bat
@@ -7,7 +7,7 @@ echo %SDK_ROOT%
 
 if "%1" == "setenv" goto setenv
 
-%comspec% /k %SDK_ROOT%SDKShell.bat setenv %1
+%comspec% /k "%SDK_ROOT%SDKShell.bat" setenv %1
 goto exit
 
 :setenv
@@ -26,16 +26,16 @@ goto setenv2
 
 :hideoci
 @echo Hiding the OCI plugin library.
-if not exist %SDK_ROOT%bin\gdal\plugins-optional mkdir %SDK_ROOT%bin\gdal\plugins-optional
-if exist %SDK_ROOT%bin\gdal\plugins\ogr_OCI.dll move %SDK_ROOT%bin\gdal\plugins\ogr_OCI.dll %SDK_ROOT%bin\gdal\plugins-optional\ogr_OCI.dll
-if exist %SDK_ROOT%bin\gdal\plugins\gdal_GEOR.dll move %SDK_ROOT%bin\gdal\plugins\gdal_GEOR.dll %SDK_ROOT%bin\gdal\plugins-optional\gdal_GEOR.dll
+if not exist "%SDK_ROOT%bin\gdal\plugins-optional" mkdir "%SDK_ROOT%bin\gdal\plugins-optional"
+if exist "%SDK_ROOT%bin\gdal\plugins\ogr_OCI.dll" move "%SDK_ROOT%bin\gdal\plugins\ogr_OCI.dll" "%SDK_ROOT%bin\gdal\plugins-optional\ogr_OCI.dll"
+if exist "%SDK_ROOT%bin\gdal\plugins\gdal_GEOR.dll" move "%SDK_ROOT%bin\gdal\plugins\gdal_GEOR.dll" "%SDK_ROOT%bin\gdal\plugins-optional\gdal_GEOR.dll"
 
 :setenv2
-SET PATH=%SDK_ROOT%bin;%SDK_ROOT%bin\gdal\python\osgeo;%SDK_ROOT%bin\proj\apps;%SDK_ROOT%bin\gdal\apps;%SDK_ROOT%bin\ms\apps;%SDK_ROOT%bin\gdal\csharp;%SDK_ROOT%bin\ms\csharp;%SDK_ROOT%bin\curl;%PATH%
-SET GDAL_DATA=%SDK_ROOT%bin\gdal-data
-SET GDAL_DRIVER_PATH=%SDK_ROOT%bin\gdal\plugins
-SET PYTHONPATH=%SDK_ROOT%bin\gdal\python;%SDK_ROOT%bin\ms\python
-SET PROJ_LIB=%SDK_ROOT%bin\proj\SHARE
+SET "PATH=%SDK_ROOT%bin;%SDK_ROOT%bin\gdal\python\osgeo;%SDK_ROOT%bin\proj\apps;%SDK_ROOT%bin\gdal\apps;%SDK_ROOT%bin\ms\apps;%SDK_ROOT%bin\gdal\csharp;%SDK_ROOT%bin\ms\csharp;%SDK_ROOT%bin\curl;%PATH%"
+SET "GDAL_DATA=%SDK_ROOT%bin\gdal-data"
+SET "GDAL_DRIVER_PATH=%SDK_ROOT%bin\gdal\plugins"
+SET "PYTHONPATH=%SDK_ROOT%bin\gdal\python;%SDK_ROOT%bin\ms\python"
+SET "PROJ_LIB=%SDK_ROOT%bin\proj\SHARE"
 
 
 :exit
