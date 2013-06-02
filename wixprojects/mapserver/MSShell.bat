68c7de694ba965a4f851e8823895d22a3a01e16b
Tamas Szekeres <szekerest@gmail.com>
2013-06-02 11:52:01 +0200

    Add files for mapserver installer (#28)

---
+++ wixprojects/mapserver/MSShell.bat
@@ -0,0 +1,9 @@
+@echo off
+@echo Setting environment for using the GDAL and MapServer Utilities.
+set SDK_ROOT=%~dp0
+set SDK_ROOT=%SDK_ROOT:\\=\%
+SET "PATH=%SDK_ROOT%;%PATH%"
+SET "GDAL_DATA=%SDK_ROOT%gdal-data"
+SET "GDAL_DRIVER_PATH=%SDK_ROOT%gdalplugins"
+SET "PROJ_LIB=%SDK_ROOT%projlib"
+SET "PYTHONPATH=%SDK_ROOT%python"
