4d9888a9d11d2849dac3da161eb84d41d81c048c
Tamas Szekeres <szekerest@gmail.com>
2013-09-24 19:57:48 +0200

    Upgrade the stable builds to mapserver-6-4 branch (#37)

--- setversions.bat
+++ setversions.bat
@@ -1,12 +1,12 @@
-set ms_version=6-2
-set ms_rel=1
-set ms_rev=3b531be271a1a01701e92eb7029d1914df466872
+set ms_version=6-4
+set ms_rel=0
+set ms_rev=a098e546885dd7244ce6174ec11e6f0410879a41     
 
 set gdal_major=1
 set gdal_minor=10
 set gdal_stable_tag=110
 set gdal_dev_tag=110
-set gdal_rel=0
-set gdal_rev=25947
+set gdal_rel=1
+set gdal_rev=26374
 
 set gdal_version=%gdal_major%.%gdal_minor%
