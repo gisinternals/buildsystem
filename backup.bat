bc03b418ca32d4dff69cd5b34a3436f2b6e120fe
Tamas Szekeres <szekerest@gmail.com>
2014-12-14 00:29:48 +0100

    backup files to 7z instead of zip

--- backup.bat
+++ backup.bat
@@ -1,4 +1,5 @@
-if exist C:\Backup_E\Builds2.zip del C:\Backup_E\Builds2.zip
-if exist C:\Backup_E\Builds.zip rename C:\Backup_E\builds.zip Builds2.zip
-7z a -tzip C:\Backup_E\Builds.zip E:\Builds
-copy /Y C:\Backup_E\Builds.zip \\ts_hmei\share\Backup
+if exist C:\Backup_E\Builds2.7z del C:\Backup_E\Builds2.7z
+if exist C:\Backup_E\Builds.7z rename C:\Backup_E\builds.7z Builds2.7z
+rem 7z a -tzip C:\Backup_E\Builds.zip E:\Builds
+7z a C:\Backup_E\Builds.7z E:\Builds
+copy /Y C:\Backup_E\Builds.7z \\ts_hmei\share\Backup
