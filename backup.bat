e9c3ae22f66d1d0ecdd19915cae764a716dadd97
Tamas Szekeres <szekerest@gmail.com>
2017-07-09 19:15:26 +0200

    upgrade pixman/fontconfig/cairo

--- backup.bat
+++ backup.bat
@@ -2,4 +2,4 @@ if exist C:\Backup_E\Builds2.7z del C:\Backup_E\Builds2.7z
 if exist C:\Backup_E\Builds.7z rename C:\Backup_E\builds.7z Builds2.7z
 rem 7z a -tzip C:\Backup_E\Builds.zip E:\Builds
 7z a C:\Backup_E\Builds.7z E:\Builds >C:\Backup_E\backup.txt 2>C:\Backup_E\backup_err.txt
-copy /Y C:\Backup_E\Builds.7z Z:\backup
+rem copy /Y C:\Backup_E\Builds.7z Z:\backup
