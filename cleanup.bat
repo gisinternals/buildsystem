200e1c6b486e2088c7d3e1f7e29e45b290a645ae
Tamas Szekeres <szekerest@gmail.com>
2018-05-07 19:49:27 +0200

    makefile tweaks

--- cleanup.bat
+++ cleanup.bat
@@ -1,157 +1,157 @@
 cd E:\sdk\vc9\mapserver
-git clean
+git clean -f
 cd E:\sdk\vc9\msautotest
-svn cleanup
+git clean -f
 cd E:\sdk\vc9\mapserver-7-0
-git clean
+git clean -f
 cd E:\sdk\vc9\msautotest-7-0
-git clean
-cd E:\sdk\vc9\gdal-trunk
-svn cleanup
+git clean -f
+cd E:\sdk\vc9\gdal
+git clean -f
 cd E:\sdk\vc9\gdal-2.2
-svn cleanup
+git clean -f
 
 cd E:\sdk\vc9x64\mapserver
-git clean
+git clean -f
 cd E:\sdk\vc9x64\msautotest
-git clean
+git clean -f
 cd E:\sdk\vc9x64\mapserver-7-0
-git clean
+git clean -f
 cd E:\sdk\vc9x64\msautotest-7-0
-git clean
-cd E:\sdk\vc9x64\gdal-trunk
-svn cleanup
+git clean -f
+cd E:\sdk\vc9x64\gdal
+git clean -f
 cd E:\sdk\vc9x64\gdal-2.2
-svn cleanup
+git clean -f
 
 cd E:\sdk\vc10\mapserver
-git clean
+git clean -f
 cd E:\sdk\vc10\msautotest
-git clean
+git clean -f
 cd E:\sdk\vc10\mapserver-7-0
-git clean
+git clean -f
 cd E:\sdk\vc10\msautotest-7-0
-git clean
-cd E:\sdk\vc10\gdal-trunk
-svn cleanup
+git clean -f
+cd E:\sdk\vc10\gdal
+git clean -f
 cd E:\sdk\vc10\gdal-2.2
-svn cleanup
+git clean -f
 
 cd E:\sdk\vc10x64\mapserver
-git clean
+git clean -f
 cd E:\sdk\vc10x64\msautotest
-git clean
+git clean -f
 cd E:\sdk\vc10x64\mapserver-7-0
-git clean
+git clean -f
 cd E:\sdk\vc10x64\msautotest-7-0
-git clean
-cd E:\sdk\vc10x64\gdal-trunk
-svn cleanup
+git clean -f
+cd E:\sdk\vc10x64\gdal
+git clean -f
 cd E:\sdk\vc10x64\gdal-2.2
-svn cleanup
+git clean -f
 
 cd E:\sdk\vc11\mapserver
-git clean
+git clean -f
 cd E:\sdk\vc11\\msautotest
-git clean
+git clean -f
 cd E:\sdk\vc11\\mapserver-7-0
-git clean
+git clean -f
 cd E:\sdk\vc11\msautotest-7-0
-git clean
-cd E:\sdk\vc11\gdal-trunk
-svn cleanup
+git clean -f
+cd E:\sdk\vc11\gdal
+git clean -f
 cd E:\sdk\vc11\gdal-2.2
-svn cleanup
+git clean -f
 
 cd E:\sdk\vc11x64\mapserver
-git clean
+git clean -f
 cd E:\sdk\vc11x64\msautotest
-git clean
+git clean -f
 cd E:\sdk\vc11x64\mapserver-7-0
-git clean
+git clean -f
 cd E:\sdk\vc11x64\msautotest-7-0
-git clean
-cd E:\sdk\vc11x64\gdal-trunk
-svn cleanup
+git clean -f
+cd E:\sdk\vc11x64\gdal
+git clean -f
 cd E:\sdk\vc11x64\gdal-2.2
-svn cleanup
+git clean -f
 
 cd E:\sdk\vc12\mapserver
-git clean
+git clean -f
 cd E:\sdk\vc12\msautotest
-git clean
+git clean -f
 cd E:\sdk\vc12\mapserver-7-0
-git clean
+git clean -f
 cd E:\sdk\vc12\msautotest-7-0
-git clean
-cd E:\sdk\vc12\gdal-trunk
-svn cleanup
+git clean -f
+cd E:\sdk\vc12\gdal
+git clean -f
 cd E:\sdk\vc12\gdal-2.2
-svn cleanup
+git clean -f
 
 cd E:\sdk\vc12x64\mapserver
-git clean
+git clean -f
 cd E:\sdk\vc12x64\msautotest
-git clean
+git clean -f
 cd E:\sdk\vc12x64\mapserver-7-0
-git clean
+git clean -f
 cd E:\sdk\vc12x64\msautotest-7-0
-git clean
-cd E:\sdk\vc12x64\gdal-trunk
-svn cleanup
+git clean -f
+cd E:\sdk\vc12x64\gdal
+git clean -f
 cd E:\sdk\vc12x64\gdal-2.2
-svn cleanup
+git clean -f
 
 cd E:\sdk\vc14\mapserver
-git clean
+git clean -f
 cd E:\sdk\vc14\msautotest
-git clean
+git clean -f
 cd E:\sdk\vc14\mapserver-7-0
-git clean
+git clean -f
 cd E:\sdk\vc14\msautotest-7-0
-git clean
-cd E:\sdk\vc14\gdal-trunk
-svn cleanup
+git clean -f
+cd E:\sdk\vc14\gdal
+git clean -f
 cd E:\sdk\vc14\gdal-2.2
-svn cleanup
+git clean -f
 
 cd E:\sdk\vc14x64\mapserver
-git clean
+git clean -f
 cd E:\sdk\vc14x64\msautotest
-git clean
+git clean -f
 cd E:\sdk\vc14x64\mapserver-7-0
-git clean
+git clean -f
 cd E:\sdk\vc14x64\msautotest-7-0
-git clean
-cd E:\sdk\vc14x64\gdal-trunk
-svn cleanup
+git clean -f
+cd E:\sdk\vc14x64\gdal
+git clean -f
 cd E:\sdk\vc14x64\gdal-2.2
-svn cleanup
+git clean -f
 
 cd E:\sdk\vc15\mapserver
-git clean
+git clean -f
 cd E:\sdk\vc15\msautotest
-git clean
+git clean -f
 cd E:\sdk\vc15\mapserver-7-0
-git clean
+git clean -f
 cd E:\sdk\vc15\msautotest-7-0
-git clean
-cd E:\sdk\vc15\gdal-trunk
-svn cleanup
+git clean -f
+cd E:\sdk\vc15\gdal
+git clean -f
 cd E:\sdk\vc15\gdal-2.2
-svn cleanup
+git clean -f
 
 cd E:\sdk\vc15x64\mapserver
-git clean
+git clean -f
 cd E:\sdk\vc15x64\msautotest
-git clean
+git clean -f
 cd E:\sdk\vc15x64\mapserver-7-0
-git clean
+git clean -f
 cd E:\sdk\vc15x64\msautotest-7-0
-git clean
-cd E:\sdk\vc15x64\gdal-trunk
-svn cleanup
+git clean -f
+cd E:\sdk\vc15x64\gdal
+git clean -f
 cd E:\sdk\vc15x64\gdal-2.2
-svn cleanup
+git clean -f
 
 cd e:\builds
--- makevc.bat
+++ makevc.bat
@@ -1,14 +1,16 @@
 @echo off
 set WIX_LIGHT_OPTS=-sval
-echo starting makevc9.bat
-cmd /c makevc9.bat
-echo starting makevc10.bat
-cmd /c makevc10.bat
-echo starting makevc11.bat
-cmd /c makevc11.bat
-echo starting makevc12.bat
-cmd /c makevc12.bat
-echo starting makevc14.bat
-cmd /c makevc14.bat
 echo starting makevc15.bat
 cmd /c makevc15.bat
+echo starting makevc14.bat
+cmd /c makevc14.bat
+echo starting makevc12.bat
+cmd /c makevc12.bat
+echo starting makevc11.bat
+cmd /c makevc11.bat
+echo starting makevc10.bat
+cmd /c makevc10.bat
+echo starting makevc9.bat
+cmd /c makevc9.bat
+
+
