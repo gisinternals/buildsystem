2d63ae5eb9ebf37ade32525341cae8434ef27a9a
Tamas Szekeres <szekerest@gmail.com>
2018-10-21 17:23:20 +0200

    update release versions

--- setversions.bat
+++ setversions.bat
@@ -1,14 +1,14 @@
 set ms_version=7-2
-set ms_rel=0
-set ms_rev=8644369f39575c7640d6ad19c8052d1e16b516ee           
+set ms_rel=1
+set ms_rev=d5c22cd3113ba85f5b1f1c406fe4055f75d04ce3       
 set ms_osgeo4w_version=7.0.7
-set ms_osgeo4w_version_dev=7.1.0
+set ms_osgeo4w_version_dev=7.2.1
 
 set gdal_major=2
 set gdal_minor=3
 set gdal_stable_tag=203
 set gdal_dev_tag=203
-set gdal_rel=1
-set gdal_rev=2c866d3c62bb52852d7ab6850b63d3a3d81b51a1
+set gdal_rel=2
+set gdal_rev=4ebe3d0fffa547556d9fe18a17f51b1e77a2849b
 
 set gdal_version=%gdal_major%.%gdal_minor%
