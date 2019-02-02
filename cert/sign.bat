e5b2b0c852a69950a4fc671ea52c0887dcbbfeaa
Tamas Szekeres <szekerest@gmail.com>
2019-02-02 21:42:26 +0100

    Update signer

--- cert/sign.bat
+++ cert/sign.bat
@@ -1,1 +1,1 @@
-signtool sign /sha1 1A1BD22A1C7D8A9BC13453BED8DC33C7EB370274 /fd SHA256 /n "Open Source Developer, Tamas Szekeres" /t http://time.certum.pl/ /v "%1"
+signtool sign /sha1 38a83e3ed7181b409427d5af9e8b8d8a8e80860d /fd SHA256 /n "Open Source Developer, Tamas Szekeres" /t http://time.certum.pl/ /v "%1"
