a7c91998c25357e66111f2f1d95949973a08895d
Tamas Szekeres <szekerest@gmail.com>
2019-06-02 22:54:24 +0200

    prepare for gdal 3.0.0

--- setversions.bat
+++ setversions.bat
@@ -1,14 +1,14 @@
-set ms_version=7-2
-set ms_rel=2
-set ms_rev=7fe9b2b9f74796731cba2f7f09c8f41875f020e4      
+set ms_version=7-4
+set ms_rel=0
+set ms_rev=b5c26cc372dd95f80f01b741cf9f2ebbdb221733      
 set ms_osgeo4w_version=7.0.7
 set ms_osgeo4w_version_dev=7.2.2
 
-set gdal_major=2
-set gdal_minor=4
-set gdal_stable_tag=204
-set gdal_dev_tag=204
+set gdal_major=3
+set gdal_minor=0
+set gdal_stable_tag=300
+set gdal_dev_tag=300
 set gdal_rel=1
-set gdal_rev=e8cfc11f414650e5c128b2b630d8b22de70fe968
+set gdal_rev=b2e5f04239ccc3a12c3114d7f3c86ce47ec4b742
 
 set gdal_version=%gdal_major%.%gdal_minor%
