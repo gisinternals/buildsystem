39fbc09f3b9b25ca379a17259f8692587adb4fbe
Tamas Szekeres <szekerest@gmail.com>
2020-11-16 18:58:57 +0100

    update gdal version

--- setversions.bat
+++ setversions.bat
@@ -1,6 +1,6 @@
 set ms_version=7-6
 set ms_rel=1
-set ms_rev=74ae370a132f68ebeb60b48567e174cf9be2b5c1     
+set ms_rev=74ae370a132f68ebeb60b48567e174cf9be2b5c1
 set ms_osgeo4w_version=7.0.7
 set ms_osgeo4w_version_dev=7.2.2
 
@@ -8,7 +8,7 @@ set gdal_major=3
 set gdal_minor=1
 set gdal_stable_tag=301
 set gdal_dev_tag=301
-set gdal_rel=3
-set gdal_rev=1be35bb911e08985260e9721b884fc0ec90be386
+set gdal_rel=4
+set gdal_rev=f294525f1ce269db669506db100bc272871f7b9b
 
 set gdal_version=%gdal_major%.%gdal_minor%
