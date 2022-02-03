e23c9c14ed39db4d5c151e81dacd9bdb362964d7
Tamas Szekeres <szekerest@gmail.com>
2022-02-03 19:58:35 +0100

    Prepare for MSVC 2022

--- makevc.bat
+++ makevc.bat
@@ -2,6 +2,8 @@
 set WIX_LIGHT_OPTS=-sval
 echo starting makevc16.bat
 cmd /c makevc16.bat
+echo starting makevc17.bat
+cmd /c makevc17.bat
 echo starting makevc15.bat
 cmd /c makevc15.bat
 
--- makevcx64.bat
+++ makevcx64.bat
@@ -2,6 +2,8 @@
 set WIX_LIGHT_OPTS=-sval
 echo starting makevc16x64.bat
 cmd /c makevc16x64.bat
+echo starting makevc17x64.bat
+cmd /c makevc17x64.bat
 echo starting makevc15x64.bat
 cmd /c makevc15x64.bat
 
