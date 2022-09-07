c29831e26374a36ad598f29bb2de07bc2422e1ac
Tamas Szekeres <szekerest@gmail.com>
2022-09-07 21:05:08 +0200

    update versions

--- makepackage.bat
+++ makepackage.bat
@@ -278,7 +278,7 @@ echo ^<span style="background-color:Lime;color:Black;font-weight:bold"^>%cmdname
 echo ^<span style="background-color:Lime;color:Black;font-weight:bold"^>Success^</span^>^<br/^> >>C:\Inetpub\wwwroot\sdk\build-output\%logid%.html
 )
 
-set nmakecmd=op-disable PKG_VERSION=%pkg-version% INSTALL_DIR=C:\Inetpub\wwwroot\sdk\downloads
+set nmakecmd=package-libs PKG_VERSION=%pkg-version% INSTALL_DIR=C:\Inetpub\wwwroot\sdk\downloads
 set cmdname=Packaging headers and libs
 for /f "tokens=1-7 delims=:.,- " %%a in ("%date% %time%") do set id=%%a%%b%%c-%%d-%%e-%%f-%%g-%logid%
 echo %date% %time%: %cmdname% started ^&nbsp^<a href="%baseurl%%compiler%-%id%.txt"^>stdout^</a^>^&nbsp^<a href="%baseurl%%compiler%-%id%-err.txt"^>stderr^</a^>^&nbsp >>C:\Inetpub\wwwroot\sdk\build-output\%logid%.html
