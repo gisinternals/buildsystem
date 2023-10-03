b40e96759893b98d5c1943cc040ed59ce92bab18
Tamas Szekeres <szekerest@gmail.com>
2023-10-03 14:39:59 +0200

    update gdal release

--- setversions.bat
+++ setversions.bat
@@ -5,7 +5,7 @@ set gdal_major=3
 set gdal_minor=7
 set gdal_stable_tag=307
 set gdal_dev_tag=307
-set gdal_rel=1
+set gdal_rel=2
 
 set gdal_version=%gdal_major%-%gdal_minor%
 set gdal-release-ver=%gdal_major%.%gdal_minor%.%gdal_rel%
