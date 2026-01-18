713950e938f40f5dc3cda90688e7659c95e62861
Tamas Szekeres <szekerest@gmail.com>
2026-01-18 01:34:24 +0100

    updates

--- cert/sign.bat
+++ cert/sign.bat
@@ -1,1 +1,1 @@
-signtool sign /sha1 5c0914c9eb4ccdb67ad4e05e93e5a8b54623d63e /fd SHA256 /t http://time.certum.pl/ /v "%1"
+signtool sign /sha1 c34335831cd48077484114fa0445b2727127f195 /fd SHA256 /t http://time.certum.pl/ /v "%1"
